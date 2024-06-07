9f144fe46f26f80f356305577b95da07573ac0b7
Inderpal001 <inderpal.pixlerlab@gmail.com>
2024-06-07 16:13:48 +0530

    hadsup

---
+++ app/Components/Banner/Banner.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Banner.scss";
+import Places from "../Places/Places";
+import Calendar from "../Calendar/Calendar";
+
+
+export default function Banner() {
+  return (
+    <>
+      <Navbar />
+      <div className="banner">
+        <div className="banner-content">
+          <h1 className="banner-heading">Find Your Ad Spots</h1>
+          <h3 className="banner-small-heading">Advertise in your local</h3>
+
+          <div className="banner-input-box-div">
+              <div className="up-row">
+                  <div className="banner-input-box">
+                      <p className="banner-input-text">I want to advertise in...</p>
+                      <input type="text" className="banner-input left-banner-input border-left-radius" placeholder="Search Ad Spots..."/>
+                      <div className="place-suggestions">
+                          <p className="place-heading">Popular Locations</p>
+                          <div className="places-grid">
+                              <Places names="E, London"/>
+                              <Places names="N, London"/>
+                              <Places names="NW, London"/>
+                              <Places names="WC, London"/>
+                              <Places names="W1, London"/>
+                              <Places names="SE, London"/>
+                              <Places names="SW, London"/>
+                              <Places names="W, London"/>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="banner-input-box">
+                      <p className="banner-input-text">My campaign will run...</p>
+                      <input type="text" className="banner-input right-banner-input border-right-radius" placeholder="Select compaign dates"/>
+                      <div className="calendar-div">
+                        <Calendar/>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="down-row">
+                  <p className="banner-input-text">Advanced Filter</p>
+                  <button disabled className="banner-search-btn">Search</button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Banner from './Components/Banner/Banner'
+
+export default function page() {
+  return (
+    <div><Banner/></div>
+  )
+}
