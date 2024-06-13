02371ec431b7e193a1e6bcd73fd0f6aea274a143
Inderpal001 <inderpal.pixlerlab@gmail.com>
2024-06-13 13:02:45 +0530

    input change

--- app/Components/MiuiCalendar/MiuiCalendar.jsx
+++ app/Components/MiuiCalendar/MiuiCalendar.jsx
@@ -2,15 +2,59 @@
 
 import "./MiuiCalendar.scss";
 import * as React from 'react';
-import dayjs from 'dayjs';
+import { Input } from '@mui/base/Input';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function ResponsiveDatePickers() {
+function ButtonField(props) {
+  const {
+    setOpen,
+    id,
+    disabled,
+    InputProps: { ref } = {},
+    inputProps: { 'aria-label': ariaLabel } = {},
+  } = props;
+
+  return (
+    <Input type="text" readOnly
+      variant="outlined"
+      id={id}
+      disabled={disabled}
+      ref={ref}
+      aria-label={ariaLabel}
+      onClick={() => setOpen?.((prev) => !prev)}
+      placeholder="Select campaign dates"
+     >
+    </Input>
+  );
+}
+
+function ButtonDatePicker(props) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <DatePicker
+      slots={{ ...props.slots, field: ButtonField }}
+      slotProps={{ ...props.slotProps, field: { setOpen } }}
+      {...props}
+      open={open}
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+    />
+  );
+}
+
+export default function PickerWithButtonField() {
+  const [value, setValue] = React.useState(null);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <MobileDatePicker className="calendar-input" defaultValue={dayjs('2022-04-17')} />    
+      <ButtonDatePicker
+        label={value == null ? null : value.format('MM/DD/YYYY')}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      />
     </LocalizationProvider>
   );
 }
